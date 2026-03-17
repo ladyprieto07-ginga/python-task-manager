1768e0c604325dca72cbae7fc7d0771e5b79f5f5
Lady Prieto <ladyprieto07@gmail.com>
2026-03-16 23:31:42 -0500

    Added task saving feature

--- task_manager.py
+++ task_manager.py
@@ -1,43 +1,60 @@
-tasks = []
+import json
 
-while True:
-    print("\n===== TASK MANAGER =====")
+
+
+try:
+    with open("tasks.json", "r") as file:
+        tasks = json.load(file)
+except:
+    tasks = []
+    
+def save_tasks():
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file)
+        
+def show_menu():
+    print("\n--- TASK MANAGER ---")
     print("1. Add task")
     print("2. View tasks")
     print("3. Delete task")
     print("4. Exit")
 
+def add_task():
+    save_tasks()
+    task = input("Enter a new task: ")
+    tasks.append(task)
+    print("Task added!")
+
+def view_tasks():
+    if len(tasks) == 0:
+        print("No tasks available.")
+    else:
+        print("\nYour tasks:")
+        for i, task in enumerate(tasks):
+            print(f"{i + 1}. {task}")
+
+def delete_task():
+    view_tasks()
+    save_tasks()
+    try:
+        task_number = int(input("Enter the task number to delete: "))
+        tasks.pop(task_number - 1)
+        print("Task deleted!")
+    except:
+        print("Invalid task number.")
+
+while True:
+    show_menu()
     choice = input("Choose an option: ")
 
     if choice == "1":
-        task = input("Enter the task: ")
-        tasks.append(task)
-        print("Task added successfully!")
-
+        add_task()
     elif choice == "2":
-        if len(tasks) == 0:
-            print("No tasks available.")
-        else:
-            print("\nYour tasks:")
-            for i, task in enumerate(tasks):
-                print(f"{i + 1}. {task}")
-
+        view_tasks()
     elif choice == "3":
-        if len(tasks) == 0:
-            print("No tasks to delete.")
-        else:
-            for i, task in enumerate(tasks):
-                print(f"{i + 1}. {task}")
-            try:
-                num = int(input("Enter the task number to delete: "))
-                tasks.pop(num - 1)
-                print("Task deleted!")
-            except:
-                print("Invalid option.")
-
+        delete_task()
     elif choice == "4":
         print("Goodbye!")
         break
-
     else:
-        print("Invalid option. Try again.")
+        print("Invalid option.")
